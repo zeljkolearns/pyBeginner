7a3dcf6e48937aa1debd57c9d1bea1b5d257246a
codeNIH <codenih@gmail.com>
2016-03-15 19:09:12 -0400

    Rename ex3.py to ex03.py

---
+++ ex03.py
@@ -0,0 +1,2 @@
+# Exercise 3
+## Objective: prompt a user to enter their age; script should output "Your age is [age here]"
--- ex3.py
+++
@@ -1,2 +0,0 @@
-# Exercise 3
-## Objective: prompt a user to enter their age; script should output "Your age is [age here]"
