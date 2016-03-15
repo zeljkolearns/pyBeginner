ac6bbd49489d24f3e47446f86fd30b5280ea9bf8
codeNIH <codenih@gmail.com>
2016-03-15 19:09:35 -0400

    Rename ex5.py to ex05.py

---
+++ ex05.py
@@ -0,0 +1,2 @@
+# Exercise 5
+## Objective: create a function combine(word1,word2); when run, the function should return a portmanteau of the inputted parameters
--- ex5.py
+++
@@ -1,2 +0,0 @@
-# Exercise 5
-## Objective: create a function combine(word1,word2); when run, the function should return a portmanteau of the inputted parameters
