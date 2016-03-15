72af50318d0d07cc01bb0d7e1243096cd5bd55b1
codeNIH <codenih@gmail.com>
2016-03-15 19:09:27 -0400

    Rename ex4.py to ex04.py

---
+++ ex04.py
@@ -0,0 +1,2 @@
+# Exercise 4
+## Objective: create a function dog(); when run, this function should return "poodle" 
--- ex4.py
+++
@@ -1,2 +0,0 @@
-# Exercise 4
-## Objective: create a function dog(); when run, this function should return "poodle" 
