6084f3b08820a1c8beb8e6b0185fa0ac4a0faad3
zeljkolearns <ti.to@blic.net>
2024-08-02 12:59:59 +0200

    excercise 3

---
+++ Answers/ex03 - answer.py
@@ -0,0 +1,5 @@
+# Exercise 3
+## Objective: prompt a user to enter their age; script should output "Your age is [age here]"
+
+age  = input("How many years do you have?\n")
+print(f"You have {age} years!!!")
